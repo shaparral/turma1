6b1beaea790482a8e1d04bbc7c06c4d0f4386269
Rafael Zerbini <rafinhazerbini@gmail.com>
2018-12-13 13:39:28 -0200

    Calculator

--- App.js
+++ App.js
@@ -1,41 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { isUndefined } from 'util';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      nome: '',
-      idade: ''
+      valor1: '',
+      valor2: '',
+      resultado: 0
     };
 }
 
+calculadora(x, y, operacao) {
+  let resultado = '';
+  if(operacao === 'soma') {
+    resultado = parseInt(x, 10) + parseInt(y, 10)
+  } else if(operacao === 'sub') {
+    resultado = parseInt(x, 10) - parseInt(y, 10)
+  } else {
+    resultado = 'Operação inválida'
+  }
+  this.setState({resultado});
+}
+
   soma(x, y) {
-    return x + y;
+    const resultado = parseInt(x, 10) + parseInt(y, 10);
+    if(resultado === 0) {
+      this.setState({resultado: 'Coloque só valores positivos!'});
+    } else {
+      this.setState({ resultado });
+    }
   }
  
   render() {
     return (
       <View style={styles.container}>
-      <TextInput
-        style={{height: 100, borderColor: 'gray', 
-        borderWidth: 1, 
-        width: '100%', fontSize: 40}}
-        placeholder='Digite seu nome:'
-        onChangeText={(nome) => this.setState({nome})}
-        value={this.state.nome}
-      />
-        <Text style={{ fontSize: 30 }}>
-        {`Nome: ${this.state.nome}`}
-        </Text>
         <TextInput
-        style={{height: 100, borderColor: 'gray', borderWidth: 1, width: '100%', fontSize: 40}}
-        placeholder='Digite sua idade:'
-        onChangeText={(idade) => this.setState({idade})}
-        value={this.state.idade}
-      />
-      <Text style={{ fontSize: 30 }}>{`Idade: ${this.state.idade}`}</Text>
-
+          style={{height: 100, borderColor: 'gray', 
+          borderWidth: 1, 
+          width: '100%', fontSize: 40}}
+          placeholder='Digite seu nome:'
+          onChangeText={(valor1) => this.setState({valor1})}
+          value={this.state.valor1}
+          keyboardType='numeric'
+        />
+        <Text style={{ fontSize: 30 }}>
+        {`Nome: ${this.state.valor1}`}
+        </Text>
+         <TextInput
+          style={{height: 100, borderColor: 'gray', 
+          borderWidth: 1, width: '100%', fontSize: 40}}
+          placeholder='Digite sua idade:'
+          onChangeText={(valor2) => this.setState({valor2})}
+          value={this.state.valor2}
+          keyboardType='numeric'
+        />
+        <Text style={{ fontSize: 30 }}>
+        {`Idade: ${this.state.valor2}`}
+        </Text>
+        <Button 
+          onPress={() => this.calculadora(this.state.valor1, this.state.valor2, 'soma')}
+          title='+'
+          color="#841584"
+          />
+           <Button 
+          onPress={() => this.calculadora(this.state.valor1, this.state.valor2, 'sub')}
+          title='-'
+          color="#841584"
+          />
+        <Text style={{ fontSize: 30 }}>
+        {`Resultado: ${this.state.resultado}`}
+        </Text>
       </View>
     );
   }
